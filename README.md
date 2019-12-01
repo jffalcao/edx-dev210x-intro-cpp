20b689490e67e6f705f8acccf2a3f0b442b2c80d
José Fernando Falcao <jfribeirofalcao@gmail.com>
2019-12-01 11:52:21 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # edx-dev210x-intro-cpp
+
+https://courses.edx.org/courses/course-v1:Microsoft+DEV210x+3T2019/course/
---
+++ mod2/intro-variables/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // int i{2};
+    int i = 2;
+    i = 3.2;
+    i = 2.9;
+    i = -1;
+    
+    // unsigned int u{0};
+    unsigned int u = 0;
+    u = -2;
+    
+    // double d{2.7};
+    double d = 2.7;
+    i = d;
+    d = i;
+    
+    bool flag = true;
+    flag = false;
+    flag = 7;
+    
+    return 0;
+}
